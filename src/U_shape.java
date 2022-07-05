2da2fb97d258bcf7ff55717801218417ffe9aac6
16Kharal_G <kharalgirish@gmail.com>
2022-07-05 10:16:03 +0100

    con plan u shape

--- src/U_shape.java
+++ src/U_shape.java
@@ -15,10 +15,13 @@ public class U_shape extends Classroom{
         }
         Pupil blank = new Pupil(" " , " " , true);
         //Todo put blank inbetween and fininsh the sorting algorithm
-        for( int x = 0 ; x < sideRowLength; x = x +2) {
-            for (int i = x; i < backRowLength - 1; i++) {
+        for( int x = 0 ; x <(sideRowLength * (backRowLength + 2)) - 1 ; x = x + (backRowLength + 2)){
+            for (int i = x; i < x + (backRowLength -1); i++) {
                 PlanLL.add(blank, i + 1 );//not done
             }
+        }
+        for (int p = (sideRowLength * (backRowLength + 2)) ; p <(sideRowLength * (backRowLength + 2)) + backRowLength-1 ; p++ ){
+            PlanLL.add(blank, p + 1  );//not done
         }
     }
 }
